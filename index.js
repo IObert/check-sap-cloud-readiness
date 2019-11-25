6d6cbfb98ca5f8b9ab7013624ec94ac62aa74364
Marius Obert <marius.obert@sap.com>
2019-11-25 17:13:51 +0100

    refactor code

--- index.js
+++ index.js
@@ -9,41 +9,13 @@ const Fs = require('fs');
 const Path = require('path');
 const Exec = Util.promisify(require('child_process').exec);
 
+const { VersionStringParser } = require('./helpers');
+
 //TODO add tests, test correct output, test correct detection, test suggestions, eslint for code qualitity
 //TODO logic to merge requirements if several options are used + update dependecies if necessary
 //TODO add a way to re-use already defined dependencies
 
 const sleep = require('util').promisify(setTimeout)
-
-//Helpers
-String.prototype.grepFirstLine = function (sTerm) {
-  return this.split(/\n/).find((s) => s.indexOf(sTerm) > -1)
-};
-const reVersionString = /(\d+\.)?(\d+\.)(\*|\d+)/;
-var bClear = true;
-var aMissingTools = [];
-var aTools = []
-
-//Check input parameters
-if (process.argv.length <= 2) {
-  console.log(`Please select an option. Possible values are:\n\r`)
-
-  Fs.readdir(Path.join(__dirname, 'options'), function (err, files) {
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-    files.forEach(function (file) {
-      console.log(file.replace(/\.js/, ''));
-    });
-
-    console.log(`\n\rExample: npx check-sap-cloud-readiness -codejam-cap`)
-  });
-  return;
-}
-process.argv.slice(2).forEach(function (val) {
-  let aNewTools = require('./options/' + val.replace('-', ''));
-  aTools = aTools.concat(aNewTools);
-});
 
 //Define output table, spaces needed to center column headings
 const oTable = new Table({
@@ -55,58 +27,100 @@ const oTable = new Table({
   ],
   colWidths: [30, 13, 13, 13]
 });
+const aMissingTools = [];
 
-async function startCheck() {
-  // await sleep(4000);
+
+//Check input parameters
+if (process.argv.length <= 2) {
+  console.error(`Please select an option. Possible values are:\n\r`)
+  let sample;
+
+  Fs.readdir(Path.join(__dirname, 'options'), function (err, files) {
+    if (err) {
+      return console.error('Unable to scan directory: ' + err);
+    }
+    files.forEach(function (file) {
+      sample = file.replace(/\.js/, '');
+      console.error(sample);
+    });
+
+    console.error(`\n\rExample: npx check-sap-cloud-readiness -${sample}`)
+  });
+  return
+}
+
+(async function startCheck() {
+  // await sleep(4000); //can be used for debugging
   console.log(Chalk.bgBlackBright.cyan('Performing check now... \n\r\n\r'));
-  const aPromises = aTools
-    .filter((oTool) => {
-      return oTool.platform ? oTool.platform === process.platform : true; // pass if platform prooperty isn't defined or equal to the current platform
-    }).map((oTool) => {
+
+  const aCommandPromises = triggerAllCommands();
+  Promise.all(aCommandPromises)
+    .then(printSummary);
+})();
+
+
+function getComponentsToCheck() {
+  let aTools = [];
+  process.argv.slice(2).forEach(function (val) {
+    let aNewTools = require('./options/' + val.replace('-', ''));
+    aTools = aTools.concat(aNewTools);
+  });
+  // check if target platform is defined and applies to the current platform
+  return aTools.filter((oTool) => {
+    return oTool.platform ? oTool.platform === process.platform : true;
+  });
+}
+
+function triggerAllCommands() {
+  return getComponentsToCheck()
+    .map((oTool) => {
       const oLine = [oTool.name, "-", oTool.minVersion || "-", Chalk.bold.red('Missing!')];
       oTable.push(oLine)
       return Exec(oTool.command)
-        .then((oResult) => {
+        .then(function (oResult) {
           sUnparsed = oTool.parser ? oResult.stdout.grepFirstLine(oTool.parser) : oResult.stdout;
           if (oTool.skipVersion && sUnparsed) {
             oLine[2] = '-';
             oLine[3] = Chalk.bold.green('OK');
-            return;
-          }
-          if (reVersionString.test(sUnparsed)) {
-            let iInstalledVersion = sUnparsed.match(reVersionString)[0];
-            oLine[1] = iInstalledVersion;
-            if (CompareVersions(iInstalledVersion, oTool.minVersion) >= 0) {
-              oLine[3] = Chalk.bold.green('OK');
-            } else {
-              bClear = false;
-              oLine[3] = Chalk.bold.yellow('Outdated!');
-              aMissingTools.push(oTool);
-            }
+          } else if (VersionStringParser.test(sUnparsed)) {
+            compareVersion(oLine, oTool);
           } else {
-            bClear = false;
             aMissingTools.push(oTool);
           }
-        }).catch(() => {
-          bClear = false;
-          aMissingTools.push(oTool);
+        }).catch((oError) => {
+          if (oError.message.indexOf("Command failed:") >= 0) {
+
+            aMissingTools.push(oTool);
+            return;
+          }
+          throw oError;
+
         });
     })
 
-
-  Promise.all(aPromises).then(function () {
-    console.log(oTable.toString());
-    console.log(); //add line break
-    if (bClear) {
-      console.log(Chalk.bold.green('You are good to go, great!'))
-    } else {
-      console.log(Chalk.bold.red('Not all required tools are installed and up to date, please fix these issues.'));
-      aMissingTools.forEach((oTool) => console.log(TerminalLink(oTool.name, oTool.url)));
-
-
-      console.log(Chalk.bold('If you think you installed all tools property, please check your PATH variable to make sure the tools are referenced there.'));
+  function compareVersion(oLine, oTool) {
+    let iInstalledVersion = sUnparsed.match(VersionStringParser)[0];
+    oLine[1] = iInstalledVersion;
+    if (CompareVersions(iInstalledVersion, oTool.minVersion) >= 0) {
+      oLine[3] = Chalk.bold.green('OK');
     }
-  });
+    else {
+      oLine[3] = Chalk.bold.yellow('Outdated!');
+      aMissingTools.push(oTool);
+    }
+  }
 }
 
-startCheck();
+function printSummary() {
+  console.log(oTable.toString());
+  console.log(); //add line break
+  if (aMissingTools.length === 0) {
+    console.log(Chalk.bold.green('You are good to go, great!'))
+  } else {
+    console.log(Chalk.bold.red('Not all required tools are installed and up to date, please fix these issues.'));
+    aMissingTools.forEach((oTool) => console.log(TerminalLink(oTool.name, oTool.url)));
+
+    console.log(Chalk.bold('If you think you installed all tools property, please check your PATH variable to make sure the tools are referenced there.'));
+  }
+}
+
